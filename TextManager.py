44b23e7f90d099387828b2d249492632efd2a919
cbs970524 <58094664+cbs970524@users.noreply.github.com>
2024-02-02 20:11:56 +0900

    TextManager 클래스 변경, TextType 추가
    TextManager
    1. 정규식 패턴 따로 분류
    2. 멘트 판별 함수 추가
    3. 곡 제목 판별 함수 추가
    4. 가사 가이드 판별 함수 추가
    
    TextType SONG_LIST => 곡목
    추가

--- TextManager.py
+++ TextManager.py
@@ -1,11 +1,16 @@
 from Text import Text
 from TextType import TextType
 import re
+import logging
 
 
 class TextManager:
     def __init__(self):
         self.songs = list()
+        self.patternDate = r"\d?\d[월](\s??)\d?\d[일]"
+        self.patternFileName1 = r"경배(\s*?)찬양"
+        self.patternFileName2 = r"경배와(\s*?)찬양"
+        self.patternSongTitle = r"(\d[).]|1️⃣|2️⃣|3️⃣|4️⃣|5️⃣|6️⃣|7️⃣).+(?=\n|$)"
 
     def splitText(self, text):
         splitedTexts = [t.strip() for t in text.split("\n\n") if t.strip()]
@@ -15,31 +20,44 @@ class TextManager:
         texts = []
         for i in splitedTexts:
             text = Text(i)
-            if self.isSONG_TITLE(i):
+            if self.isSONG_LIST(i):
                 self.classifySONG_TITLE(i)
-                text.setTextType(TextType.SONG_TITLE)
-            elif i.find("멘트") != -1:
+                text.setTextType(TextType.SONG_LIST)
+            elif self.isMENT_GUIDE(i):
                 text.setTextType(TextType.MENT_GUIDE)
             elif self.isFILE_NAME(i):
                 text.setTextType(TextType.FILE_NAME)
             elif self.isNeed(i):
                 continue
+            elif self.isSONG_TITLE(i):
+                text.setTextType(TextType.SONG_TITLE)
+            elif self.isLYRICS_GUIDE(i):
+                text.setTextType(TextType.LYRICS_GUIDE)
             texts.append(text)
         return texts
 
     def isFILE_NAME(self, text) -> bool:
-        return bool(re.compile("\d?\d[월](\s?)\d?\d[일]").search(text)) * (
-            bool(re.compile("경배\s?찬양").search(text))
-            + bool(re.compile("경배와\s?찬양").search(text))
+        return bool(re.search(self.patternDate, text)) * (
+            bool(re.search(self.patternFileName1, text))
+            + bool(re.search(self.patternFileName2, text))
         )
 
-    def isSONG_TITLE(self, text) -> bool:
+    def isSONG_LIST(self, text) -> bool:
         return True if "곡목" in text else False
+
+    def isMENT_GUIDE(self, text) -> bool:
+        return True if "멘트" in text else False
 
     def classifySONG_TITLE(self, text):
         self.songs = [
-            match.strip() for match in re.findall(r"(?<=\d[).]).+(?=\n|$)", text)
+            match.strip() for match in re.findall(self.patternSongTitle, text)
         ]
 
     def isNeed(self, text) -> bool:
         return True if "밴드" in text or "인도자" in text or "불참" in text else False
+
+    def isSONG_TITLE(self, text) -> bool:
+        return True if bool(re.search(self.patternSongTitle, text)) else False
+
+    def isLYRICS_GUIDE(self, text) -> bool:
+        return True if "가사" in text else False
--- TextType.py
+++ TextType.py
@@ -5,6 +5,7 @@ class TextType(Enum):
     FILE_NAME = auto()
     MENT = auto()
     MENT_GUIDE = auto()
+    SONG_LIST = auto()
     SONG_TITLE = auto()
     LYRICS = auto()
     LYRICS_GUIDE = auto()
