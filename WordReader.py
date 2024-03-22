0f62f1e47b3fed799476ba0918c9137f12db9800
cbs970524 <58094664+cbs970524@users.noreply.github.com>
2024-03-22 19:17:14 +0900

    기본 텍스트 체크 메서드
    강조 표시 없는 기본 텍스트인지 체크.

--- WordReader.py
+++ WordReader.py
@@ -3,6 +3,7 @@ import os
 
 
 class WordReader:
+    # ◎◆◇
     def openfile(self, file_name):
         # 바탕화면 경로 가져오기
         desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
@@ -24,3 +25,30 @@ class WordReader:
             print("파일이 없습니다.")
 
         return doc
+
+    def check_default(self, paragraph):
+        for run in paragraph.runs:
+            if run.bold != None:
+                return False
+            if run.underline != None:
+                return False
+            if run.color != None:
+                return False
+        return True
+        # 스타일, 기울임, 폰트, 폰트 사이즈 어떻게 할지 고민.
+
+
+"""# 각 문단에 대한 정보 출력
+for paragraph in doc.paragraphs:
+    print("Text:", paragraph.text)
+    for run in paragraph.runs:
+        print("run.text:",run.text)
+        print("  Style:", run.style.name)
+        print("  Bold:", run.bold)
+        print("  Italic:", run.italic)
+        print("  Underline:", run.underline)
+        print("  Color:", run.font.color.rgb if run.font.color else None)
+        print("  Font:", run.font.name)
+        print("  Font Size:", run.font.size.pt if run.font.size else None)
+    print("  -------------------------\n")
+    print("  -------------------------\n")"""
